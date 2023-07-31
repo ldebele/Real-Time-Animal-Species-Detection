ecbec1a0e76fefb76c78a5c52218878c704050f6
ldebele <lemidebele@gmail.com>
2023-07-31 16:51:19 +0300

    completed train test split function

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -5,17 +5,9 @@ import random
 
 BASE_DIR = '../data'
 RAW_DIR = os.path.join(BASE_DIR, 'raw')
-IMG_DIR = os.path.join(BASE_DIR, 'images')
-LABEL_DIR = os.path.join(BASE_DIR, 'labels')
-TRAIN_IMG_PATH = os.path.join(IMG_DIR, 'train')
-TRAIN_LABEL_PATH = os.path.join(LABEL_DIR, 'train')
-VAL_IMG_PATH = os.path.join(IMG_DIR, 'val')
-VAL_LABEL_PATH = os.path.join(LABEL_DIR, 'val')
-TEST_IMG_PATH = os.path.join(IMG_DIR, 'test')
 
 
-
-def split_images_labels(all_files, subfile, name, IMG_DIR, LABEL_DIR=None):
+def images_labels_split(all_files, subfile, name, mode):
     """
         Args:
             all_files: list   
@@ -28,32 +20,28 @@ def split_images_labels(all_files, subfile, name, IMG_DIR, LABEL_DIR=None):
     data = [file for file in all_files if file[:3] in subfile]
 
     images = [img for img in data if img.endswith(".jpg")]
-    labels = [label for label in data if label.endswith(".txt")]
-
-    # create directory if not created
-    os.makedirs(IMG_DIR, exist_ok=True)
-    os.makedirs(IMG_DIR, exist_ok=True)
+    labels = [label for label in data if label.endswith(".txt")]   
 
     # Copy files to the respective folders
-    if IMG_DIR:
-        # create directory
-        os.makedirs(f"{IMG_DIR}/{name}", exist_ok=True)
-        destination = os.path.join(IMG_DIR, name)
-        try:
-            for img in images:
-                IMG_DIR = os.path.join(RAW_DIR, f"{name}/{img}")
-                shutil.copy(IMG_DIR, destination)
-        except:
-            print("Unable to save the images")
+    # create directory if not created
+    IMG_DIR = os.path.join(BASE_DIR, f'images/{mode}/{name}')
+    os.makedirs(IMG_DIR, exist_ok=True)
+    try:
+        for img in images:
+            RAW_IMG_DIR = os.path.join(RAW_DIR, f"{name}/{img}")
+            shutil.copy(RAW_IMG_DIR, IMG_DIR)
+    except:
+        print("Unable to save the images")
 
-    if LABEL_DIR:
-        # create directory
-        os.makedirs(f"{LABEL_DIR}/{name}", exist_ok=True)
-        destination = os.path.join(LABEL_DIR, name)
+
+    if mode != 'test':
+        # create directory if not created
+        LABEL_DIR = os.path.join(BASE_DIR, f'labels/{mode}/{name}')
+        os.makedirs(LABEL_DIR, exist_ok=True)
         try:
             for label in labels:
-                LABEL_DIR = os.path.join(RAW_DIR, f"{name}/{label}")
-                shutil.copy(LABEL_DIR, destination)
+                RAW_LABEL_DIR = os.path.join(RAW_DIR, f"{name}/{label}")
+                shutil.copy(RAW_LABEL_DIR, LABEL_DIR)
         except:
             print("Unable to save the labels.")
 
@@ -63,7 +51,9 @@ def train_test_split(name, split_ratio, sample=110):
         Args:
             name: str
             split_ratio: list
+            sample: int
     """
+
     # set to the raw data path
     DATA_PATH = os.path.join(RAW_DIR, name)
     files = [filename[:3] for filename in os.listdir(DATA_PATH) if filename.endswith(".txt")]
@@ -86,18 +76,16 @@ def train_test_split(name, split_ratio, sample=110):
     test_sets = files[train_size+val_size:]
 
     # split and save the dataset into images and labels
-    split_images_labels(os.listdir(DATA_PATH), train_sets, name, TRAIN_IMG_PATH, TRAIN_LABEL_PATH)  # For training set
-    split_images_labels(os.listdir(DATA_PATH), val_sets, name, VAL_IMG_PATH, VAL_LABEL_PATH)  # For validation set
-    split_images_labels(os.listdir(DATA_PATH), test_sets, name, TEST_IMG_PATH)  # For test set
+    images_labels_split(os.listdir(DATA_PATH), train_sets, name, mode="train")  # For training set
+    images_labels_split(os.listdir(DATA_PATH), val_sets, name, mode="val")  # For validation set
+    images_labels_split(os.listdir(DATA_PATH), test_sets, name, mode="test")  # For test set
     
 
 if __name__ == "__main__":
     # set split ratio
     split_ratio = [0.8, 0.15]  # 80% train, 15% validation, 5% test
 
-    # split the raw data into train, validation and test sets.
-    train_test_split("buffalo", split_ratio)
-    train_test_split("elephant", split_ratio)
-    train_test_split("rhino", split_ratio)
-    train_test_split("zebra", split_ratio)
-
+    animals = ["buffalo", "elephant", "rhino", "zebra"]
+    for animal in animals:
+        # split the raw data into train, validation and test sets.
+        train_test_split(animal, split_ratio)
